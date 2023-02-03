c5638fcc0326f943573417b591e60b0ec74fe8e7
Andrea Bedini <andrea.bedini@tweag.io>
2023-02-03 16:10:25 +0800

    Simple flake setup

---
+++ flake.nix
@@ -0,0 +1,71 @@
+{
+  inputs = {
+    nixpkgs.follows = "haskell-nix/nixpkgs-unstable";
+    haskell-nix.url = "github:input-output-hk/haskell.nix";
+    flake-utils.follows = "haskell-nix/flake-utils";
+    CHaP = {
+      url = "github:input-output-hk/cardano-haskell-packages?ref=repo";
+      flake = false;
+    };
+    iohk-nix = {
+      url = "github:input-output-hk/iohk-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, haskell-nix, CHaP, iohk-nix }:
+
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          inherit (haskell-nix) config;
+          overlays = [
+            haskell-nix.overlay
+            iohk-nix.overlays.crypto
+          ];
+        };
+
+        inherit (pkgs) lib;
+
+        project = pkgs.haskell-nix.cabalProject' {
+          src = ./.;
+          compiler-nix-name = "ghc8107";
+
+          inputMap = {
+            "https://input-output-hk.github.io/cardano-haskell-packages" = CHaP;
+          };
+
+          modules = [
+            {
+              packages = {
+                # Broken due to haddock errors. Refer to https://github.com/input-output-hk/plutus/blob/master/nix/pkgs/haskell/haskell.nix
+                plutus-ledger.doHaddock = false;
+                plutus-use-cases.doHaddock = false;
+
+                # See https://github.com/input-output-hk/iohk-nix/pull/488
+                cardano-crypto-praos.components.library.pkgconfig = lib.mkForce [ [ pkgs.libsodium-vrf ] ];
+                cardano-crypto-class.components.library.pkgconfig = lib.mkForce [ [ pkgs.libsodium-vrf ] ];
+              };
+            }
+          ];
+
+        };
+
+        flake = project.flake { };
+
+      in
+      flake
+    );
+
+  nixConfig = {
+    extra-substituters = [
+      "https://cache.iog.io"
+      "https://cache.zw3rk.com"
+    ];
+    extra-trusted-public-keys = [
+      "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
+      "loony-tools:pr9m4BkM/5/eSTZlkQyRt57Jz7OMBxNSUiMC4FkcNfk="
+    ];
+  };
+}
